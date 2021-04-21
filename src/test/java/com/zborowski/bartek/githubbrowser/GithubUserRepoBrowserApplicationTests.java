d7c8f192512b9180c1ef9896cf37ca2fc6dc8be2
Bartosz Zborowski <zborowski.bartek@gmail.com>
2021-04-21 19:32:41 +0200

    Added initial commit

---
+++ src/test/java/com/zborowski/bartek/githubbrowser/GithubUserRepoBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zborowski.bartek.githubbrowser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUserRepoBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
